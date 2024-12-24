546575fbf1fafd6389f81f55fa698f300605b842
OlegPazny <petr.jakimovets@gmail.com>
2024-12-24 15:59:29 +0300

    переделал кормилку сотрудников

--- assets/api/update_survey.php
+++ assets/api/update_survey.php
@@ -22,57 +22,62 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $sheet = $spreadsheet->getActiveSheet();
                 $data = $sheet->toArray();
                 mysqli_query($db, "TRUNCATE TABLE `employees`");
+                // Инициализируем массив для подсчета
+                $department_count = array_fill(1, 18, 0);
                 foreach ($data as $index => $row) {
                     if (strpos($row[8], "Внештатный")!==false) {
                         continue;
                     }
-                    $department_id=NULL;
                     if($row[3]=="Департамент инноваций"){
-                        $department_id=1;
+                        $department_count[1]++;
                     }else if($row[3]=="Технический департамент"){
-                        $department_id=2;
+                        $department_count[2]++;
                     }else if($row[3]=="Производственный департамент"&&strpos($row[4], "Группы разработки")!==false&&strpos($row[5], "Группа выпуска документации")===false){
-                        $department_id=3;
+                        $department_count[3]++;
                     }else if($row[3]=="Производственный департамент"&&strpos($row[4], "Отдел тестирования")!==false){
-                        $department_id=4;
+                        $department_count[4]++;
                     }else if($row[3]=="Производственный департамент"&&(strpos($row[4], "Служба сопровождения")!==false||(strpos($row[5], "Служба сопровождения")!==false))){
-                        $department_id=5;
+                        $department_count[5]++;
                     }else if($row[3]=="Производственный департамент"&&strpos($row[4], "Группы разработки")!==false&&strpos($row[5], "Группа выпуска документации")!==false){
-                        $department_id=6;
+                        $department_count[6]++;
                     }else if($row[3]=="Производственный департамент"&&strpos($row[4], "Отдел тестирования")===false&&strpos($row[4], "Служба сопровождения")===false&&strpos($row[4], "Группы разработки")===false&&strpos($row[5], "Группа выпуска документации")===false){
-                        $department_id=7;
+                        $department_count[7]++;
                     }else if($row[3]=="Департамент развития бизнеса и продуктов"&&strpos($row[7], "Аккаунт-менеджер")!==false){
-                        $department_id=8;
+                        $department_count[8]++;
                     }else if($row[3]=="Департамент развития бизнеса и продуктов"&&strpos($row[7], "Пресейл консультант")!==false){
-                        $department_id=9;
+                        $department_count[9]++;
                     }else if($row[3]=="Департамент развития бизнеса и продуктов"&&strpos($row[7], "Пресейл консультант")===false&&strpos($row[7], "Аккаунт-менеджер")===false){
-                        $department_id=10;
+                        $department_count[10]++;
                     }else if($row[3]=="Департамент бизнес-анализа"){
-                        $department_id=11;
+                        $department_count[11]++;
                     }else if($row[3]=="Проектный офис"&&strpos($row[7], "Менеджер разработки")!==false){
-                        $department_id=12;
+                        $department_count[12]++;
                     }else if($row[3]=="Проектный офис"&&strpos($row[7], "Менеджер внедрения")!==false){
-                        $department_id=13;
+                        $department_count[13]++;
                     }else if($row[3]=="Проектный офис"&&strpos($row[4], "Отдел внедрения")!==false){
-                        $department_id=14;
+                        $department_count[14]++;
                     }else if($row[3]=="Служба персонала"){
-                        $department_id=15;
+                        $department_count[15]++;
                     }else if($row[3]=="Служба бизнес-процессов"){
-                        $department_id=16;
+                        $department_count[16]++;
                     }else if($row[3]=="Бэк-офис"){
-                        $department_id=17;
+                        $department_count[17]++;
                     }
                     if(strpos($row[7], "Руководитель")!==false){
-                        $department_id=18;
-                    }
-                    if ($department_id !== null) {    
-                        $stmt = $db->prepare("INSERT INTO employees (id, department_id) VALUES (NULL, ?)");
-                        $stmt->bind_param("i", $department_id);
-                        $stmt->execute();
+                        $department_count[18]++;
                     }
                 }
+                echo("<pre>");
+                var_dump($department_count);
+                echo("</pre>");
+                // Обновляем данные в таблице departments
+                foreach ($department_count as $department_id => $count) {
+                    $stmt = $db->prepare("UPDATE departments SET current = ? WHERE id = ?");
+                    $stmt->bind_param("ii", $count, $department_id);
+                    $stmt->execute();
+                }
 
-                echo "Данные успешно обработаны!";
+                echo "Данные успешно обновлены.";
             } else {
                 throw new Exception("Файл не загружен корректно.");
             }
